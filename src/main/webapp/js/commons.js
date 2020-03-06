a275553fc696484cefda4f6b622bc0db948bbed4
Matthew Jiang <myj99@utexas.edu>
2020-03-06 03:00:06 -0600

    added navbar to homepage

---
+++ src/main/webapp/js/commons.js
@@ -0,0 +1,17 @@
+function insertSidebar(){
+    elmt = document.getElementById("sidebar");
+    elmt.innerHTML='\
+    <div class="row">\
+        <a href="homepage.html"><img class="img-fluid my-3" src="images/logo.png"><\a>\
+        <form class="form-inline my-2 my-lg-0">\
+            <input class="form-control mr-sm-0" type="search" placeholder="Search" aria-label="Search">\
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">🔎</button>\
+        </form>\
+        <hr>\
+        <ul class="list-unstyled">\
+        <li><a href="">Home</a></li><br>\
+        <li><a href="">About</a></li><br>\
+        <li><a href="">Surprise Me</a></li><br>\
+        </ul>\
+    </div>';
+}
