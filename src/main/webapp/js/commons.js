1e11ccafbc681f3a7b64e8d5ab4631583c88263c
sshetkar3858 <sshetkar@utexas.edu>
2020-03-06 21:13:26 -0600

    made edits to homepage so text looks realistic and modified design of navbar

--- src/main/webapp/js/commons.js
+++ src/main/webapp/js/commons.js
@@ -1,17 +1,15 @@
 function insertSidebar(){
     elmt = document.getElementById("sidebar");
     elmt.innerHTML='\
-    <div class="row">\
         <a href="homepage.html"><img class="img-fluid my-3" src="images/logo.png"><\a>\
         <form class="form-inline my-2 my-lg-0">\
             <input class="form-control mr-sm-0" type="search" placeholder="Search" aria-label="Search">\
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">🔎</button>\
         </form>\
         <hr>\
-        <ul class="list-unstyled">\
-        <li><a href="homepage.html">Home</a></li><br>\
-        <li><a href="AboutPage.html">About</a></li><br>\
-        <li><a href="">Surprise Me</a></li><br>\
-        </ul>\
-    </div>';
+        <div class="list-group">\
+        <a href="homepage.html" class="list-group-item list-group-item-action">Home</a>\
+        <a href="AboutPage.html" class="list-group-item list-group-item-action">About</a>\
+        <a href="homepage.html" class="list-group-item list-group-item-action">Surprise Me</a>\
+        </div>'; 
 }
