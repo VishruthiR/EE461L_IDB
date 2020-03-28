3cc14e9687758cdc0ca35c0fdeef559c8fb26781
Jaino Vennatt <jainovennatt@Jainos-MacBook-Pro.local>
2020-03-28 16:03:53 -0500

    populate has proper parameters, still have to connect to mongo and push json objects

--- backEndScripts/populate.py
+++ backEndScripts/populate.py
@@ -8,9 +8,11 @@ print('Author List Name:', str(sys.argv[1]))
 fileName = sys.argv[1]
 genre = sys.argv[2]
 
-
 with open(fileName, "r") as a_file:
+    numberOfBooksAdded=0
+
     for line in a_file:                         #checking list of Authors
+
         authorName=line.rstrip("\n")
         params = dict(
             maxResults = '1',
@@ -51,13 +53,40 @@ with open(fileName, "r") as a_file:
                 book=currentBooks[indexOfCurrentBookInLoop]
                 innerbook= book['volumeInfo']
                 if('industryIdentifiers' in innerbook and 'imageLinks' in innerbook) :
-                    innerbook2=innerbook['industryIdentifiers']
-                    val=innerbook2[0]
+                    if('authors'not in innerbook):
+                        continue
+                    authors=innerbook['authors']
+                    if(authorName not in authors):
+                        continue
+                    innerbook['authors']=authorName
+
+                    alreadyHadRating = 1
+                    if('averageRating' not in innerbook):
+                       (book['volumeInfo'])['averageRating'] = 0.0
+                       alreadyHadRating= 0
+
+                    if(alreadyHadRating==1):
+                        (book['volumeInfo'])['numberOfRatings'] = 10.0
+                    elif(alreadyHadRating==0):
+                        (book['volumeInfo'])['numberOfRatings'] = 0.0
+
+                    print(innerbook['averageRating'], 'and quantity:', innerbook['numberOfRatings'])
+                    val=(innerbook['industryIdentifiers'])[0]
                     secondvalue =val['type']
+
                     if(secondvalue == 'ISBN_10' or secondvalue == 'ISBN_13'):
+
+                        if(secondvalue=='ISBN_10'):
+                            innerbook['industryIdentifiers']=(innerbook['industryIdentifiers'])[1]
+
+                        elif(secondvalue=='ISBN_13'):
+                            innerbook['industryIdentifiers']=(innerbook['industryIdentifiers'])[0]
+
                         (book['volumeInfo'])['genre'] = genre
 
                         json_formatted_str = json.dumps(book, indent=2)
+                        numberOfBooksAdded=numberOfBooksAdded+1
+                        print('Current number of Books: ', numberOfBooksAdded)
                         print(json_formatted_str)
                         #print('\n\n')
             #print('\n')
