358ac4477f8f0cb12a894431323db5d8f98979e1
davidday99 <david.day@utexas.edu>
2020-03-29 17:50:14 -0500

    Moved picture from header to description

--- test_react_book/test_react_book/src/components2/Description.jsx
+++ test_react_book/test_react_book/src/components2/Description.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import Grid from '@material-ui/core/Grid';
 
 class Description extends React.Component {
   render() {
     return (
       <Card>
         <CardContent>
-          <Typography variant="h6">{this.props.description}</Typography>
+		  <Grid container>
+			  <Grid item sm={2} container>
+			    <img src={this.props.image} style={{flexDirection: 'row', flexWrap: 'wrap', width: 'inherit', height: 'inherit'}} />
+			  </Grid>
+			  <Grid item sm={10}>
+          	    <Typography variant="h6" style={{padding: 10}}>{this.props.description}</Typography>
+		  	  </Grid>
+		  </Grid>
         </CardContent>
       </Card>
     );
   }
 }
+
 export default Description;
--- test_react_book/test_react_book/src/components2/Header.jsx
+++ test_react_book/test_react_book/src/components2/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
-
+import Paper from '@material-ui/core/Paper';
+import { lineHeight } from '@material-ui/system';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
 
 class Header extends React.Component {
 	render() {
-		return <Grid container>
-			<Grid item sm>
-				<img src={this.props.image} />
-			</Grid>
-			<Grid item sm>
+		return <Paper style={{padding: 20, fontSize: 36}}>
+			<div style={{padding: 10}}>
 				<h1>{this.props.title}</h1>
-			</Grid>
-		</Grid>
+				<h3 style={{lineHeight: 0}}>by David Day</h3>
+			</div>
+		</Paper>
 	}
 }
 
