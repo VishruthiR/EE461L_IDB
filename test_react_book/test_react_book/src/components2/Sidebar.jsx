a7e82008cc4c9648437a0a8d2b32e9c9a276dbe6
Matthew Jiang <myj99@utexas.edu>
2020-03-30 00:53:55 -0500

    Sidebar

---
+++ test_react_book/test_react_book/src/components2/Sidebar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import {Link, NavLink} from 'react-router-dom';
+
+
+class Sidebar extends React.Component {
+	render() {
+		return(
+			<Drawer
+				variant="permanent"
+				anchor="left"
+			>
+				<Link to="/index.html"><img src={this.props.logoSmall}/></Link>
+				<Divider />
+				<FormControl>
+					<InputLabel htmlFor="search-bar">Search...</InputLabel>
+					<Input id="search-bar" />
+				</FormControl>
+				<Divider />
+				<List>
+					<ListItem button component={NavLink} to="/index.html" activeStyle={{
+						fontWeight: "bold"
+					}}>
+						<ListItemText primary="Home">
+						</ListItemText>
+					</ListItem>
+					<ListItem button component={NavLink} to="/books" activeStyle={{
+						fontWeight: "bold"
+					}}>>
+						<ListItemText primary="Books">
+						</ListItemText>
+					</ListItem>
+					<ListItem button component={NavLink} to="/authors" activeStyle={{
+						fontWeight: "bold"
+					}}>>
+						<ListItemText primary="Authors">
+						</ListItemText>
+					</ListItem>
+					<ListItem button component={NavLink} to="/genres" activeStyle={{
+						fontWeight: "bold"
+					}}>>
+						<ListItemText primary="Genres">
+						</ListItemText>
+					</ListItem>
+				</List>
+			</Drawer>
+		)
+	}
+}
+
+export default Sidebar;
--- test_react_book/test_react_book/src/index.js
+++ test_react_book/test_react_book/src/index.js
@@ -6,12 +6,14 @@ import Author from "./components2/Author";
 import Genre from "./components2/Genre";
 import Results from "./components2/Results";
 import Homepage from "./components2/Homepage";
+import Sidebar from "./components2/Sidebar";
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <BrowserRouter>
+	  <Sidebar />
           <Switch>
             <Route path="/" exact component={Homepage} />
             <Route path="/book/:ISBN" component={Book} />
