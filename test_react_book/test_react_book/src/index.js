453ab0fd40134cf1bf916c10db65b07755c51771
sshetkar3858 <sshetkar@utexas.edu>
2020-03-30 12:30:37 -0500

    added sidebar back  in

--- test_react_book/test_react_book/src/index.js
+++ test_react_book/test_react_book/src/index.js
@@ -6,21 +6,36 @@ import Author from "./components2/Author";
 import Genre from "./components2/Genre";
 import Results from "./components2/Results";
 import Homepage from "./components2/Homepage";
-import About from "./components2/About"
+import About from "./components2/About";
+import Sidebar from "./components2/Sidebar";
+import Box from "@material-ui/core/Box";
+import ResultsP from "./components2/ResultsP";
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <BrowserRouter>
-          <Switch>
-            <Route path="/" exact component={Homepage} />
-            <Route path="/book/:ISBN" component={Book} /> 
-            <Route path="/author/" component={Author} />
-            <Route path="/genre/:name" component={Genre} />
-            <Route path="/results/:query" component={Results} />
-            <Route path="/about" component={About} />
-          </Switch>
+          <Box display="flex">
+            <div style={{ flexShrink: 0, width: 240 }}>
+              <Sidebar />
+            </div>
+            <div style={{ flexGrow: 1 }}>
+              <Switch>
+                <Route path="/" exact component={Homepage} />
+                <Route path="/about" exact component={About} />
+                <Route path="/book/:ISBN" component={Book} />
+                <Route path="/author/:authorName" component={Author} />
+                <Route path="/genre/:name" component={Genre} />
+                <Route
+                  path="/results/:type/:query/:pageNum"
+                  component={Results}
+                />
+                //Parameter Style
+                <Route path="/results" component={ResultsP} />
+              </Switch>
+            </div>
+          </Box>
         </BrowserRouter>
       </React.Fragment>
     );
